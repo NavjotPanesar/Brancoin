c842679936cb61f5c5837c8e2a95e267d666adff
Navjot Panesar <NavjotSPanesar@gmail.com>
2024-05-22 15:22:37 -0400

    updated viewpacks to show remaining packs

--- bot/discord/commands/viewpacks.py
+++ bot/discord/commands/viewpacks.py
@@ -10,9 +10,10 @@ from discord import Message
 import discord
 import discord.ext
 import discord.ext.commands
+from sqlalchemy import func
 from discord.drawutils import DrawUtils
 from models.dbcontainer import DbService
-from models.models import BoosterCard, BoosterPack, Card, Guild, OwnedCard, Shop, User
+from models.models import BoosterCard, BoosterPack, BoosterSegment, Card, Guild, OwnedCard, Shop, User
 from discord.basecommand import BaseCommand
 import random
 
@@ -30,14 +31,23 @@ class ViewPacks(BaseCommand):
             if packs is None or len(packs) == 0:
                 await message.reply("can't find any packs")
                 return
+            
+            source = session.query(User).filter(User.user_id == str(message.author.id), User.guild_id == str(message.guild.id)).first()
+            owned_card_ids = [x.card_id for x in source.owned_cards]
+            missing_cards_text = []
+            for pack in packs:
+                distinct_cards_in_pack = session.query((func.distinct(BoosterCard.card_id))).filter(BoosterCard.booster_pack_id == pack.id).all()
+                distinct_card_ids = [x.tuple()[0] for x in distinct_cards_in_pack]
+                missing_card_ids = [x for x in distinct_card_ids if x not in owned_card_ids]
+                missing_cards_text.append(f"\nYou're missing {len(missing_card_ids)} cards from this pack!")
 
             embed = discord.Embed(title=f"Pack Shop!", description="", color=0xccffff)
             embed.set_author(name="Check out these boosters!", icon_url="https://i.imgur.com/L4Ps6O5.png")
             
-            for pack in packs:
+            for idx, pack in enumerate(packs):
                 embed.add_field(name="Pack", value=str(pack.id), inline=True)
                 embed.add_field(name="Cost", value=str(pack.cost), inline=True)
-                embed.add_field(name="Description", value=str(pack.desc), inline=True)
+                embed.add_field(name="Description", value=f"{str(pack.desc)} {missing_cards_text[idx]}" , inline=True)
                 embed.add_field(name="", value="", inline=False)
 
             embed.set_image(url="https://i.imgur.com/NifcNgd.jpeg")
