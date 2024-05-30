6b861c50785653e2e8293ef827bc4746db92c986
Navjot Panesar <NavjotSPanesar@gmail.com>
2024-05-30 01:18:58 -0400

    update web server

--- bot/webserver/web.py
+++ bot/webserver/web.py
@@ -2,6 +2,7 @@ import base64
 from io import BytesIO
 import PIL
 from bottle import Bottle, route, request 
+from discord.drawutils import DrawUtils
 from models.models import Card
 import models
 from models.dbcontainer import DbContainer, DbService
@@ -31,22 +32,8 @@ def get_image(dbservice: DbService = Provide[DbContainer.service]):
 
     with dbservice.Session() as session:
         card = session.query(Card).filter(Card.id == id).first()
-        print(card.description)
-        input_data = {
-            "card": card.card_style,
-            "Title": card.title,
-            "attribute": card.attribute,
-            "Level": int(card.level),
-            "Type": card.type,
-            "Descripton": str(card.description).replace('\\n','\n'),
-            "Atk": card.atk,
-            "Def": card.defe
-            }
-        input_data["image_card"] = PIL.Image.open(BytesIO(card.image.bin))
-        output = CardConstructor(input_data)
         response.set_header('Content-type', 'image/png')
-        output_card = output.generateCard()
-        return BytesIO(output_card)
+        return DrawUtils.card_to_byte_image(card)
 
     return "done"
 
